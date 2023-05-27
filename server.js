fc788081eb5ac4a09711fe8b719ccc81bb473fce
T0mer007 <tomerbenaim1@gmail.com>
2023-05-27 13:51:59 +0300

    second touch

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const corsOptions = {
     origin: [
         'http://127.0.0.1:8080',
         'http://localhost:8080',
-        'http://127.0.0.1:3001',
-        'http://localhost:3001',
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
     ],
     credentials: true
 }
